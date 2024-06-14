6fba4d6109faaff8f6875a5371e32a3717ca9f6d
Exoalam <nafiul.arpon@gmail.com>
2024-06-14 22:38:29 +0600

    update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,18 +3,27 @@ import React from 'react'
 const About = () => {
   return (
     <div data-theme='light' className='flex flex-row w-full min-h-screen'>
-      <div class="grid grid-cols-2 gap-4 mx-auto">
-        <div className='m-[20%]'>
-          <h1 class="text-5xl font-bold">We are Resto Robotics</h1>
+      <div className="grid grid-cols-2 gap-10 m-auto">
+        <div className='w-1/2 ml-auto'>
+          <h1 className="text-5xl font-bold ml-10">We are Resto Robotics</h1>
           <div className="relative">
             <img
               src="Assets/images/miso-robotics-team-2048x1536.jpeg"
               alt="Your Image"
-              className="w-full h-auto transition duration-500 ease-in-out transform hover:scale-110 hover:shadow-lg"
+              className="w-full h-auto transition duration-500 ease-in-out transform hover:scale-110 hover:shadow-lg filter grayscale m-10"
             />
           </div>
         </div>
-        <div>09</div>
+        <div className='w-1/2 mr-auto'>
+          <div className="relative">
+            <img
+              src="Assets/images/miso-robotics-team-2048x1536.jpeg"
+              alt="Your Image"
+              className="w-full h-auto transition duration-500 ease-in-out transform hover:scale-110 hover:shadow-lg filter grayscale m-10"
+            />
+          </div>
+          <h1 className="text-5xl font-bold ml-10">We are Resto Robotics</h1>
+        </div>
       </div>
     </div>
   )
