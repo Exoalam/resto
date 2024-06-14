07cd966e21e728ea10be4e0951fc10223e50d4bd
Exoalam <nafiul.arpon@gmail.com>
2024-06-14 22:41:18 +0600

    update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -17,12 +17,12 @@ const About = () => {
         <div className='w-1/2 mr-auto'>
           <div className="relative">
             <img
-              src="Assets/images/miso-robotics-team-2048x1536.jpeg"
+              src="Assets/images/Miso-Robotics-Team-at-Work-qimlt6uv7bxdf7l37nhas6h8ipeb47fwjfpbjvdoqw.jpeg"
               alt="Your Image"
               className="w-full h-auto transition duration-500 ease-in-out transform hover:scale-110 hover:shadow-lg filter grayscale m-10"
             />
           </div>
-          <h1 className="text-5xl font-bold ml-10">We are Resto Robotics</h1>
+          <h1 className="text-md ml-10">Resto was founded on the idea of creating robots that eliminate dull, dirty, and dangerous tasks in restaurant kitchens. Our logo was inspired by the one task that makes people cry the most: slicing onions.</h1>
         </div>
       </div>
     </div>
