04edd233dd2e48b690b0a4e2b27a0b252da13e0c
Exoalam <nafiul.arpon@gmail.com>
2024-06-15 16:09:15 +0600

    update

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
 
@@ -7,7 +8,7 @@ const News = () => {
     <div data-theme='light'>
       <div className='grid grid-cols-3 gap-y-10 w-[70%] m-auto pt-10 pb-10'>
         <div className="card w-96 bg-base-100 shadow-md hover:shadow-2xl hover:scale-[101%]">
-          <figure><img src="Assets/images/Homepage Images/news (1).png" alt="Shoes" /></figure>
+          <figure><Image src="Assets/images/Homepage Images/news (1).png" alt="Shoes" /></figure>
           <div className="card-body">
             <div className='flex flex-row'><FontAwesomeIcon icon={faClock} className='h-5 mr-2'></FontAwesomeIcon>
               <b>September 05, 2024</b>
@@ -16,27 +17,27 @@ const News = () => {
           </div>
         </div>
         <div className="card w-96 bg-base-100 shadow-md hover:shadow-2xl hover:scale-[101%]">
-          <figure><img src="Assets/images/Homepage Images/news (2).png" alt="Shoes" /></figure>
+          <figure><Image src="Assets/images/Homepage Images/news (2).png" alt="Shoes" /></figure>
           <div className="card-body">
             <div className='flex flex-row'><FontAwesomeIcon icon={faClock} className='h-5 mr-2'></FontAwesomeIcon>
               <b>September 04, 2024</b>
             </div>
-            <p>VD Company selected as 'Advanced Restaurant Industry Automation Solution Company' 
-            by the Korean Food Promotion Agency's Overseas Korean Restaurant Council Workshop.</p>
+            <p>VD Company selected as Advanced Restaurant Industry Automation Solution Company
+            by the Korean Food Promotion Agencys Overseas Korean Restaurant Council Workshop.</p>
           </div>
         </div>
         <div className="card w-96 bg-base-100 shadow-md hover:shadow-2xl hover:scale-[101%]">
-          <figure><img src="Assets/images/Homepage Images/news (3).png" alt="Shoes" /></figure>
+          <figure><Image src="Assets/images/Homepage Images/news (3).png" alt="Shoes" /></figure>
           <div className="card-body">
             <div className='flex flex-row'><FontAwesomeIcon icon={faClock} className='h-5 mr-2'></FontAwesomeIcon>
               <b>September 03, 2024</b>
             </div>
-            <p>VD Company holds the largest-ever promotion ‘Robot Festival’ 
+            <p>VD Company holds the largest-ever promotion Robot Festival
             to celebrate reaching 10,000 VD members.</p>
           </div>
         </div>
         <div className="card w-96 bg-base-100 shadow-md hover:shadow-2xl hover:scale-[101%]">
-          <figure><img src="Assets/images/Homepage Images/news (4).png" alt="Shoes" /></figure>
+          <figure><Image src="Assets/images/Homepage Images/news (4).png" alt="Shoes" /></figure>
           <div className="card-body">
             <div className='flex flex-row'><FontAwesomeIcon icon={faClock} className='h-5 mr-2'></FontAwesomeIcon>
               <b>September 02, 2024</b>
@@ -46,24 +47,22 @@ const News = () => {
           </div>
         </div>
         <div className="card w-96 bg-base-100 shadow-md hover:shadow-2xl hover:scale-[101%]">
-          <figure><img src="Assets/images/Homepage Images/news (5).png" alt="Shoes" /></figure>
+          <figure><Image src="Assets/images/Homepage Images/news (5).png" alt="Shoes" /></figure>
           <div className="card-body">
             <div className='flex flex-row'><FontAwesomeIcon icon={faClock} className='h-5 mr-2'></FontAwesomeIcon>
               <b>September 01, 2024</b>
             </div>
-            <p>VD Company ranked first among ‘Best Medium-sized and Small Businesses to Work for 
-            in the First Half of 2024’.</p>
+            <p>VD Company ranked first among Best Medium-sized and Small Businesses to Work for 
+            in the First Half of 2024.</p>
           </div>
         </div>
         <div className="card w-96 bg-base-100 shadow-md hover:shadow-2xl hover:scale-[101%]">
-          <figure><img src="Assets/images/Homepage Images/news (6).png" alt="Shoes" /></figure>
+          <figure><Image src="Assets/images/Homepage Images/news (6).png" alt="Shoes" /></figure>
           <div className="card-body">
             <div className='flex flex-row'><FontAwesomeIcon icon={faClock} className='h-5 mr-2'></FontAwesomeIcon>
               <b>August 31, 2024</b>
             </div>
-            <p>MBN's restaurant start-up survival program 'Birth of a CEO - Open War', 
-                                an emerging entertainment powerhouse... Fierce mission war for 
-                                100 million won in start-up support.</p>
+            <p>MBNs restaurant start-up survival program Birth of a CEO - Open War, an emerging entertainment powerhouse... Fierce mission war for 100 million won in start-up support.</p>
           </div>
         </div>
       </div>
