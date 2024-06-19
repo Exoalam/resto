143a32b7e31a8346c92363117096f4bbf3b3faf1
Exoalam <nafiul.arpon@gmail.com>
2024-06-19 17:16:49 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,20 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div data-theme="light" className="relative h-full">
+      {/* Mobile video */}
       <video
-        className="w-full h-full"
+        className="absolute top-0 left-0 w-full h-screen object-cover md:hidden"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="Assets/mobile.mp4" type="video/mp4" />
+      </video>
+
+      {/* Desktop video */}
+      <video
+        className="hidden md:block w-full h-full object-contain"
         autoPlay
         loop
         muted
